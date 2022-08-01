a12873b6cd0c8820b6661c40078f7356c64a87c4
rizki <rzkalqdr@gmail.com>
2022-08-01 22:57:36 +0700

    add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const moves = ['rock', 'paper', 'scissors'];
+
+// Gets computer's choice that will randomly return either 'Rock', 'Paper', or 'Scissors'. 
+// Uses the Math.random() function to pick 0, 1, and 2 and will choose the psuedo-random choice from the `moves` array.
+function getComputerChoice() {
+    return moves[Math.floor(Math.random() * 3)];
+}
