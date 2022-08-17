60b5cd5497b7d4d9ac4b8d811fb09a2fef3a5ba2
rizki <rzkalqdr@gmail.com>
2022-08-17 22:49:54 +0700

    finish

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ let resultText = '';
 
 let rounds = 1;
 
+const history = document.querySelector('.results .history');
+const fluff = document.querySelector('.results .history .fluff');
+const points = document.querySelector('.results .points');
+const winner = document.querySelector('.winner');
+
 // Gets computer's choice that will randomly return either 'Rock', 'Paper', or 'Scissors'. 
 // Uses the Math.random() function to pick 0, 1, and 2 and will choose the psuedo-random choice from the `moves` array.
 function getComputerChoice() {
@@ -20,10 +25,43 @@ function getComputerChoice() {
     return moves[Math.floor(Math.random() * (max - min + 1)) + min];
 }
 
+function decideWinner() {
+    if (playerScore === computerScore) {
+        winner.innerHTML = "It's a tie!";
+    } else if (playerScore > computerScore) {
+        winner.innerHTML = "👑 You win!";
+    } else {
+        winner.innerHTML = "💻 Computer wins!";
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    roundWinner = '';
+
+    computerMove = '';
+    playerMove = '';
+    resultText = '';
+    rounds = 1;
+
+    history.innerHTML = '';
+    points.innerHTML = 'Player: 0 <br> Computer: 0';
+    winner.innerHTML = '';
+}
+
+function titleCase(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 // Plays a single round of Rock Paper Scissors.
 // Grabs player's (`playerSelection`) and computer's (`computerSelection`) moves and compares the values to declare who is the winner of the round.
 function playRound() {
-    if (rounds > 5) return;
+    fluff.innerHTML = '';
+    if (rounds > 5) {
+        decideWinner();
+        return;
+    }
 
     let playerSelection = this.id;
     let computerSelection = getComputerChoice();
@@ -37,47 +75,38 @@ function playRound() {
             (playerSelection === 'scissors' && computerSelection === 'paper')
         ) {
             roundWinner = 'player';
-            playerScore += 1;
+            playerScore++;
         } else {
             roundWinner = 'computer';
-            computerScore += 1;
+            computerScore++;
         }
     }
 
     // Making the first letter uppercase so the string output is nicer
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+    playerSelection = titleCase(playerSelection);
+    computerSelection = titleCase(playerSelection);
 
     console.log(roundWinner, playerSelection, computerSelection);
 
     if (roundWinner === 'tie') {
-        resultText = `It's a tie! ${playerSelection} (Player) and ${computerSelection} (Computer)`;
+        resultText = `[${rounds}] It's a tie! ${playerSelection} (Player) and ${computerSelection} (Computer)`;
     } else if (roundWinner === 'player') {
-        resultText = `Player wins! ${playerSelection} (Player) beats ${computerSelection} (Computer)`;
+        resultText = `[${rounds}] Player wins! ${playerSelection} (Player) beats ${computerSelection} (Computer)`;
     } else {
-        resultText = `Computer wins! ${computerSelection} (Computer) beats ${playerSelection} (Player)`;
+        resultText = `[${rounds}] Computer wins! ${computerSelection} (Computer) beats ${playerSelection} (Player)`;
     }
 
-    const result = document.querySelector('.results .history');
-    result.innerHTML += resultText + '<br>';
+    history.innerHTML += resultText + '<br>';
+    points.innerHTML = `Player: ${playerScore} <br> Computer: ${computerScore}`;
 
     rounds++;
 }
 
-function game(rounds) {
-    console.log(`Alright! Let's play ${rounds} rounds of Rock Paper Scissors!`)
-    for (let round = 0; round < rounds; round++) {
-        console.log(`Current round: Round ${round+1}`);
-        playerMove = prompt("What's your move?").toLowerCase();
-        while (playerMove !== 'rock' && playerMove !== 'paper' && playerMove !== 'scissors') {
-            playerMove = prompt("That's not a move, silly! What's your move?").toLowerCase();
-        }
-        console.log(playRound(playerMove, getComputerChoice()));
-    }
-}
+// ---------------------------------------------------------------
 
-
-// Give every button its own event listener
 document.querySelectorAll('.buttons button').forEach((button) => {
     button.addEventListener("click", playRound);
 })
+
+const resetButton = document.querySelector('.reset button');
+resetButton.addEventListener("click", resetGame);
